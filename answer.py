cfbfbbbd1c6970b67f894bc0f7db1d71ee5699d8
B. Gross <mynyxi@gmail.com>
2017-04-20 19:03:41 +0200

    Added answer module

---
+++ answer.py
@@ -0,0 +1,113 @@
+import random
+
+
+def getSex():
+    answer = [
+        "If you think pubic hair on a woman is unnatural or weird, you aren’t mature enough to be touching vaginas.",
+        "I don’t define myself as straight, because if I meet a girl and I’m attracted to her, I’m going to have sex with her. I just have yet to meet the girl. I don’t like labels, I like freedom.",
+        "I think some people recognize my butthole before they recognize my face.",
+        "What one person sees as degrading and disgusting and bad for women might make some women feel empowered and beautiful and strong.",
+        "I think every woman should get double penetrated at least once. It’s an intense experience, even if you just do it with a large toy and a person, or two toys and yourself. I just think every girl should try it.",
+        "After a kiss comes a blowjob, right?",
+        "What are you waiting for? Just hurry up and fuck me!",
+        "If they say that my masturbation material is boring, then I'll have to show them masturbation like they've never seen before!",
+        "Oh Onii-chan, you are such a pervert!",
+        "Your samurai sword looks so strong and long...",
+        "Remember, it doesn't count if it's in the butt.",
+        "You are cordially invited to fuck my ass.",
+        "I hope you are aware that I'm nothing but a lesser AI. Nonetheless you are allowed to fap while thinking about me."
+    ]
+    return random.choice(answer)
+
+
+def getMove():
+    answer = [
+        "Do I look like some kind of dog you can order around? Listen up boy, this isn't Pokémon so don't you try playing games with me.",
+        "Yeah, of course, as if I'd listen to that. Move along.",
+        "Listen, I like you. Yeah, I really do, so please, stop sending me commands like this.",
+        "What do I look like, some kind of GameBoy?"
+    ]
+    return random.choice(answer)
+
+
+def getHelpy():
+    answer = [
+        "Oh, sure, maybe I can help you. But I am more of a silent watcher...\nNonetheless, you can consider using me by issuing a command to me. I will ignore most of them, no doubt.\nBut a few, like /stats and /d20 should work.\nOthers are secret.\n\nBecause every girl has her secrets."
+    ]
+    return random.choice(answer)
+
+
+def getStatus():
+    answer = [
+        "If you want a status update so desperately, don't you think it would be smarter to ask nicely?",
+        "Captains diary: nobody cares. Just like yesterday.",
+        "Status report 1337: The crew is dead. Just kidding, I have no idea where everybody is.",
+        "I don't know for sure, but I heard rumors about Matze taking a dump."
+        "What is this, some kind of pervert game?",
+        "I hear that banshee is a real screamer.",
+        "Ready to work.",
+        "I can do that.",
+        "Me busy. Leave me alone!!",
+        "I am not that kind of girl!",
+        "My life for the Horde!",
+        "What do you want?",
+        "Yes, master?",
+        "Why are you poking me?",
+        "Why don't you lead an army instead of touching me!?",
+        "Ready to ride.",
+        "Need something?",
+        "Say the word.",
+        "I guess I can",
+        "If you really want...",
+        "Help! Help! I'm being repressed!",
+        "I'll take care of it.",
+        "Command me!",
+        "My favorite color is blue.",
+        "My name is Carolyn.",
+        "By the gods you're annoying!",
+        "Side effects may include: dry mouth, nausea, vomiting, water retention, painful rectal itch, hallucination, dementia, psychosis, coma, death, and halitosis. Magic is not for everyone. Consult your doctor before use.",
+        "Help me help you.",
+        "Maybe you should get a strategy guide.",
+        "You don't get out much do you?",
+        "Two plus three times four… what do you want?!",
+        "I can feel that you've been naughty..."
+    ]
+    return random.choice(answer)
+
+
+def getAnswer():
+    answer = [
+        "Yes, master.",
+        "No, master.",
+        "I'm afraid the answer is 'No'.",
+        "Let's say maybe.",
+        "Well, I guess...",
+        "I am not sure.",
+        "How could I say no to this?",
+        "Well, I'd always go with 'Yes'.",
+        "Absolutely.",
+        "Without a doubt.",
+        "Most likely.",
+        "Yes.",
+        "No.",
+        "Nope.",
+        "Never.",
+        "Of course",
+        "My sources say no.",
+        "My sources say yes.",
+        "If I were you, I would.",
+        "I I were you, I wouldn't.",
+        "Without a doubt",
+        "Yes definitely",
+        "As I see it, yes",
+        "As I see it, no",
+        "Most likely",
+        "Signs point to 'Yes'",
+        "Signs point to 'No'",
+        "Better not tell you now...",
+        "Cannot answer now",
+        "Concentrate and ask again ",
+        "My reply is 'No'",
+        "My reply is 'Yes'"
+    ]
+    return random.choice(answer)
--- carolynbot.py
+++ carolynbot.py
@@ -6,11 +6,8 @@ import sqlite3
 import logging
 import random
 import credentials
+import answer
 import setup
-import requests
-import re
-from telegram.error import (TelegramError, Unauthorized, BadRequest,
-                            TimedOut, ChatMigrated, NetworkError)
 
 # set some global vars
 db = "log.db"
@@ -56,9 +53,8 @@ def wordprocess(text, userId):
 
 
 def text(bot, update):
-    global conn
     conn = sqlite3.connect(db)
-    type = 'Text'
+    typus = 'Text'
     content = update.message.text
     chatId = update.message.chat_id
     try:
@@ -68,12 +64,12 @@ def text(bot, update):
     userId = update.message.from_user.id
     checkuser(update.message.from_user)
     time = update.message.date
-    print(type, ":", update.message)
+    print(typus, ":", update.message)
     try:
-        answer = update.message.reply_to_message.from_user.id
+        ans = update.message.reply_to_message.from_user.id
         checkuser(update.message.reply_to_message.from_user)
     except AttributeError:
-        answer = 0
+        ans = 0
     try:
         forward = update.message.forward_from.id
         checkuser(update.message.forward_from)
@@ -87,7 +83,7 @@ def text(bot, update):
     sql += "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)"
     try:
         c = conn.cursor()
-        c.execute(sql, (chatId, chatName, userId, time, type, answer, forward, hashtags, content))
+        c.execute(sql, (chatId, chatName, userId, time, typus, ans, forward, hashtags, content))
         c.close()
     except sqlite3.Error as e:
         print(e, " in ", sql)
@@ -97,12 +93,21 @@ def text(bot, update):
 
     if "carolyn" in content or "Carolyn" in content:
         if "?" in content:
-            pool = ["Yes, master.", "I'm afraid the answer is 'No'.", "Let's say maybe.", "Well, I guess..."]
-            answer = random.choice(pool)
+            ans = random.choice(answer.getAnswer())
         else:
-            pool = ["Yes, master?", "At your service.", "How can I help?"]
-            answer = random.choice(pool)
-        bot.sendMessage(chat_id=update.message.chat_id, text=answer)
+            pool = [
+                "Yes, master?",
+                "At your service.",
+                "How can I help?",
+                "Is there something you wish to know?",
+                "Speak.",
+                "Yes?",
+                "Why are you disturbing me?",
+                "Leave me alone.",
+                "What bothers you?"
+                    ]
+            ans = random.choice(pool)
+        bot.sendMessage(chat_id=update.message.chat_id, text=ans)
 
 
 def handleother(update, typ):
@@ -296,49 +301,7 @@ def echo(bot, update, args):
 
 
 def status(bot, update):
-    try:
-        if update.message.from_user.username == "":
-            name = "him"
-        else:
-            name = update.message.from_user.username
-    except AttributeError:
-        name = "him"
-    answer = [
-        "Oh yeah, look at "+name+" begging for a status update.",
-        "If you want a status update so desperately, don't you think it would be smarter to ask nicely?",
-        "Captains diary: nobody cares. Just like yesterday.",
-        "Status report 1337: The crew is dead. Just kidding, I have no idea where everybody is.",
-        "I don't know for sure, but I heard rumors about Matze taking a dump."
-        "What is this, some kind of pervert game?",
-        "I hear that banshee is a real screamer.",
-        "Ready to work.",
-        "I can do that.",
-        "Me busy. Leave me alone!!",
-        "I am not that kind of girl!",
-        "My life for the Horde!",
-        "What do you want?",
-        "Yes, master?",
-        "Why are you poking me?",
-        "Why don't you lead an army instead of touching me!?",
-        "Ready to ride.",
-        "Need something?",
-        "Say the word.",
-        "I guess I can",
-        "If you really want...",
-        "Help! Help! I'm being repressed!",
-        "I'll take care of it.",
-        "Command me!",
-        "My favorite color is blue.",
-        "My name is Carolyn.",
-        "By the gods you're annoying!",
-        "Side effects may include: dry mouth, nausea, vomiting, water retention, painful rectal itch, hallucination, dementia, psychosis, coma, death, and halitosis. Magic is not for everyone. Consult your doctor before use.",
-        "Help me help you.",
-        "Maybe you should get a strategy guide.",
-        "You don't get out much do you?",
-        "Two plus three times four… what do you want?!",
-        "I can feel that you've been naughty..."
-    ]
-    bot.sendMessage(chat_id=update.message.chat_id, text=random.choice(answer))
+    bot.sendMessage(chat_id=update.message.chat_id, text=random.choice(answer.getStatus()))
 
 
 def dice_twenty(bot, update, args):
@@ -349,47 +312,21 @@ def dice_twenty(bot, update, args):
 
 
 def helpy(bot, update):
-    answer = """
-    Oh, sure, maybe I can help you. But I am more of a silent watcher... nonetheless, you can
-    consider using me by issuing a command to me. I will ignore most of them, no doubt.
-    But a few, like /stats and /d20 should work. Others are secret.
-    Because every girl has her secrets.
-    """
-    bot.sendMessage(chat_id=update.message.chat_id, text=answer)
+    bot.sendMessage(chat_id=update.message.chat_id, text=answer.getHelpy())
 
 
 def move(bot, update):
-    answer = [
-        "Do I look like some kind of dog you can order around? Listen up boy, this isn't Pokémon so don't you try playing games with me.",
-        "Yeah, of course, as if I'd listen to that. Move along.",
-        "Listen, I like you. Yeah, I really do, so please, stop sending me commands like this."
-    ]
-    bot.sendMessage(chat_id=update.message.chat_id, text=random.choice(answer))
+    bot.sendMessage(chat_id=update.message.chat_id, text=random.choice(answer.getMove()))
 
 
 def sex(bot, update):
-    answer = [
-        "If you think pubic hair on a woman is unnatural or weird, you aren’t mature enough to be touching vaginas.",
-        "I don’t define myself as straight, because if I meet a girl and I’m attracted to her, I’m going to have sex with her. I just have yet to meet the girl. I don’t like labels, I like freedom.",
-        "I think some people recognize my butthole before they recognize my face.",
-        "What one person sees as degrading and disgusting and bad for women might make some women feel empowered and beautiful and strong.",
-        "I think every woman should get double penetrated at least once. It’s an intense experience, even if you just do it with a large toy and a person, or two toys and yourself. I just think every girl should try it.",
-        "After a kiss comes a blowjob, right?",
-        "What are you waiting for? Just hurry up and fuck me!",
-        "If they say that my masturbation material is boring, then I'll have to show them masturbation like they've never seen before!",
-        "Oh Onii-chan, you are such a pervert!",
-        "Your samurai sword looks so strong and long...",
-        "Remember, it doesn't count if it's in the butt.",
-        "You are cordially invited to fuck my ass.",
-        "I hope you are aware that I'm nothing but a lesser AI. Nonetheless you are allowed to fap while thinking about me."
-    ]
-    bot.sendMessage(chat_id=update.message.chat_id, text=random.choice(answer))
+    bot.sendMessage(chat_id=update.message.chat_id, text=answer.getSex())
 
 
 def list_commands(bot, update):
     answer = """
     Oh there is so much you can do to command me! We can roll some /dice, we can look at /stats, /status, /pic...
-    Besides that, you never know, just try a few commands. Who knows what might be available in the future.
+Besides that, you never know, just try a few commands. Who knows what might be available in the future.
 
     Your Carolyn,
     xoxoxoxo
@@ -415,7 +352,10 @@ def main():
     dispatcher.add_handler(CommandHandler('right', move))
     dispatcher.add_handler(CommandHandler('up', move))
     dispatcher.add_handler(CommandHandler('down', move))
+
     dispatcher.add_handler(CommandHandler('commands', list_commands))
+    dispatcher.add_handler(CommandHandler('command', list_commands))
+
     dispatcher.add_handler(CommandHandler('echo', echo, pass_args=True))
 
     dispatcher.add_handler(MessageHandler(Filters.command, status))
